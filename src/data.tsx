ac399a2192845acfbae0ab5fb1cf265d0a53dee9
kethnal <kaseyvaldez@hotmail.com>
2023-04-06 23:06:13 -0700

    Create data file and styles files

---
+++ src/data.tsx
@@ -0,0 +1,41 @@
+const data = {
+  actions: [
+    {
+      header: "Transform your brand",
+      description: "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
+    },
+    {
+      header: "Stand out to the right audience",
+      description: "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."
+    }
+  ],
+  offers: [
+    {
+      header: "Graphic design",
+      description: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
+    },
+    {
+      header: "Photography",
+      description: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+    }
+  ],
+  testimonials: [
+    {
+      testimony: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+      name: "Emily R.",
+      title: "Marketing Director"
+    },
+    {
+      testimony: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience",
+      name: "Thomas S.",
+      title: "Chief Operating Officer"
+    },
+    {
+      testimony: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+      name: "Jenny F.",
+      title: "Business Owner"
+    }
+  ]
+}
+
+export default data;
