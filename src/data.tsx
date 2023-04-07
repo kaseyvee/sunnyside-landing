aa14429ec4b332f755991dcb16626a022e2ffcfa
kethnal <kaseyvaldez@hotmail.com>
2023-04-06 23:14:36 -0700

    Add assets

--- src/data.tsx
+++ src/data.tsx
@@ -23,17 +23,20 @@ const data = {
     {
       testimony: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
       name: "Emily R.",
-      title: "Marketing Director"
+      title: "Marketing Director",
+      image: "_general/image-emily.jpg"
     },
     {
       testimony: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience",
       name: "Thomas S.",
-      title: "Chief Operating Officer"
+      title: "Chief Operating Officer",
+      image: "_general/image-thomas.jpg"
     },
     {
       testimony: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
-      name: "Jenny F.",
-      title: "Business Owner"
+      name: "Jennie F.",
+      title: "Business Owner",
+      image: "_general/image-jenny.jpg"
     }
   ]
 }
